8dd36a61e969c543f14c2a6fa3cf220cfa8c38b4
Nikhil Rana <79563539+nikhilrana884@users.noreply.github.com>
2021-06-12 17:28:28 +0530

    Add files via upload

---
+++ Investment-Trainer/src/test/java/io/MiniProject/Investment/Trainer/InvestmentTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.MiniProject.Investment.Trainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

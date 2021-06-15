55300475498b88117ed95ade650477054e71bc12
Nikhil Rana <79563539+nikhilrana884@users.noreply.github.com>
2021-06-15 11:52:09 +0530

    Add files via upload
    Back End configured with Realtime data.

--- Investment-Trainer/src/main/java/io/MiniProject/Investment/Trainer/services/InvestmentTrainerDataService.java
+++ Investment-Trainer/src/main/java/io/MiniProject/Investment/Trainer/services/InvestmentTrainerDataService.java
@@ -1,9 +1,12 @@
 package io.MiniProject.Investment.Trainer.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -16,12 +19,41 @@ public class InvestmentTrainerDataService {
 
     @PostConstruct
     public void fetchStockData() throws IOException, InterruptedException {
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(STOCK_DATA_URL))
                 .build();
+
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
+
+        StringReader csvBodyReader = new StringReader((httpResponse.body()));
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+
+        for (CSVRecord record : records) {
+
+            String  Company = record.get("Company");
+            System.out.println(Company);
+
+            String Last_Traded_Price = record.get("LTP");
+            System.out.println(Last_Traded_Price);
+
+            String Last_Buy_Price = record.get("Buy Price");
+            System.out.println(Last_Buy_Price);
+
+            String Last_Sell_Price = record.get("Sell Price");
+            System.out.println(Last_Sell_Price);
+
+            // String  Volume = record.get("Volume");
+            // System.out.println(Volume);
+
+            // String Percent_Change = record.get("%Change");
+            // System.out.println(Percent_Change);
+
+
+
+       }
     }
 
 }
